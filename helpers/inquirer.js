c7b76f871fd2f3009d61226282a7ac4529a050eb
seraleman <seraleman1204@gmail.com>
2022-10-02 17:22:00 -0500

    Confirmar y borrar tarea

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -90,6 +90,11 @@ const listadoTareasBorrar = async (tareas = []) => {
     }
   })
 
+  choices.unshift({
+    value: '0',
+    name: '0.'.green + ' Cancelar',
+  })
+
   const preguntas = [
     {
       type: 'list',
@@ -102,9 +107,24 @@ const listadoTareasBorrar = async (tareas = []) => {
   const { id } = await inquirer.prompt(preguntas)
   return id
 }
+
+const confirmar = async (message) => {
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message,
+    },
+  ]
+
+  const { ok } = await inquirer.prompt(question)
+  return ok
+}
+
 module.exports = {
   inquirerMenu,
   pausa,
   leerInput,
   listadoTareasBorrar,
+  confirmar,
 }
