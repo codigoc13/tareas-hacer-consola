f34bf85666cab6ff8a535575cf8ac1adffa381d5
seraleman <seraleman1204@gmail.com>
2022-10-01 20:57:04 -0500

    Se cambian colores y se hace arreglo de tareas

--- models/tareas.js
+++ models/tareas.js
@@ -11,6 +11,15 @@ class Tareas {
     const tarea = new Tarea(desc)
     this._listado[tarea.id] = tarea
   }
+
+  get getListadoArr() {
+    const listado = []
+    Object.keys(this._listado).forEach((key) => {
+      const tarea = this._listado[key]
+      listado.push(tarea)
+    })
+    return listado
+  }
 }
 
 module.exports = Tareas
